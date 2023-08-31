fa5e13eb02c695919779aa87e9b59f839864c82a
9956 <zhz995617@gmail.com>
2023-08-31 15:38:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const fs = require("fs");
 const websiteUrl =
   "https://www.strava.com/athletes/119323843/activity-summary/15338ddb96e172ac63a9b54d3394dcf3c5ab360e";
 
-const screenshotPath = path.join(__dirname, `snapshot.png`);
-const previousScreenshotPath = path.join(screenshotPath, "snapshot.png");
-
-if (fs.existsSync(previousScreenshotPath)) {
-  fs.unlinkSync(previousScreenshotPath);
-  console.log("Previous screenshot deleted.");
-}
-
 (async () => {
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   await page.setViewport({ width: 480, height: 160 });
   await page.goto(websiteUrl);
+
+  const footerElement = await page.$(".footer .primary");
+  if (footerElement) {
+    const currentTime = new Date().toLocaleString();
+    await page.evaluate(
+      (element, time) => {
+        element.textContent = time;
+      },
+      footerElement,
+      currentTime
+    );
+  }
 
   const screenshotPath = path.join(__dirname, `snapshot.png`);
   await page.screenshot({
